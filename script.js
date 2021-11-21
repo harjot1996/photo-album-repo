c4f733ad26661254efb676430204134a272bd01d
harjot1996 <harjot1996@users.noreply.github.com>
2021-11-21 02:30:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $('document').ready(function () {
-  //var baseUrl_ = "https://kujqgsldke.execute-api.us-east-1.amazonaws.com/v1";
-  var baseUrl_ = "https://xak6l5w8k0.execute-api.us-east-1.amazonaws.com/v1";
+  var baseUrl_ = "https://kujqgsldke.execute-api.us-east-1.amazonaws.com/v1";
   var bucketName_ = "b2-foto";
-  var apiKey_ = "A3tizsjISU2oofgTzzCsT8wc6NOb3BDv2aAQ8i0b";
+  var apiKey_ = "IQcppMY6Vc0JL4PlTowQ8fQD90CgM424Dv4ueux7";
     
   $("#x-search-form").on('submit', function (event) {
     event.preventDefault();
