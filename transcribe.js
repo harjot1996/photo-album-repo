733203f23dfbe5eee3ff07e5701ccb6ae8cd3e98
harjot1996 <harjot1996@users.noreply.github.com>
2021-11-22 03:02:40 -0500

    Update transcribe.js

--- transcribe.js
+++ transcribe.js
@@ -1,32 +1,31 @@
 if (!('webkitSpeechRecognition' in window)) {
   console.log("Not Supported");
-  //Speech API not supported here…
 } else { //Let’s do some cool stuff :)
-  var recognition = new webkitSpeechRecognition(); //That is the object that will manage our whole recognition process. 
-  recognition.continuous = true;   //Suitable for dictation. 
-  recognition.interimResults = true;  //If we want to start receiving results even if they are not final.
-  //Define some more additional parameters for the recognition:
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
   console.log("Supported");
   recognition.lang = "en-US"; 
-  recognition.maxAlternatives = 1; //Since from our experience, the highest result is really the best...
+  recognition.maxAlternatives = 1;
 }
 
 recognition.onstart = function() {
-  //Listening (capturing voice from audio input) started.
-  //This is a good place to give the user visual feedback about that (i.e. flash a red light, etc.)
 };
 
 recognition.onend = function() {
-  //Again – give the user feedback that you are not listening anymore. If you wish to achieve continuous recognition – you can write a script to start the recognizer again here.
+  element_ = "start-button";
+  document.getElementById("start-button").style.backgroundColor = "#fff";
+  document.getElementById("start-button").style.color = "#007bff";
 };
 
-recognition.onresult = function(event) { //the event holds the results
-//Yay – we have results! Let’s check if they are defined and if final or not:
-  for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) { //Final results
-        document.getElementById("transcript").value = event.results[i][0].transcript;
-      }
-  } //end for loop
+recognition.onresult = function(event) {
+  for (var i = event.resultIndex; i < event.results.length; ++i) {      
+        if (event.results[i].isFinal) { 
+            document.getElementById("transcript").value = event.results[i][0].transcript
+        } else {
+            document.getElementById("transcript").value = event.results[i][0].transcript
+        } 
+    }
 };
 
 function startButton(event) {
@@ -36,8 +35,5 @@ function startButton(event) {
 }
 
 function stopButton(event) {
-  element_ = "start-button";
   recognition.stop();
-  document.getElementById("start-button").style.backgroundColor = "#fff";
-  document.getElementById("start-button").style.color = "#007bff";
 }
